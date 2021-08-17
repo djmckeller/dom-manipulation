7ff86b33d522ab5509455ae1ede3a9ed37e2898c
djmckeller <dariusmckeller@gmail.com>
2021-08-16 23:22:06 -0400

    dom_manipulation.js

---
+++ dom_manipulation.js
@@ -0,0 +1,24 @@
+document.getElementById("container");
+
+document.querySelector("#container");
+
+document.getElementsByClassName("second");
+
+document.querySelector("ol .third");
+
+let hello = document.querySelector("#container");
+hello.innerText = "Hello";
+
+let footer = document.querySelector(".footer");
+footer.classList.remove("main");
+
+let theList = document.querySelector("ul");
+list.appendChild(newLi);
+
+let liInsideOl = document.querySelectorAll("ol li");
+for(let i = 0; i < liInsideOl.length; i++){
+    liInsideOl[i].getElementsByClassName.backgroundColor = "green";
+}
+
+let foot = document.querySelector(".footer");
+footer.remove();
